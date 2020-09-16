bce028b98f6f8e48205e3d91de9f2721e56d84d4
ahmedmeeran.thamby <ahmedmeeran.thamby@tessolve.com>
2020-09-17 02:23:48 +0530

    Adding generateIndex and reverse methods.

--- app/src/main/java/com/ahmed/ather/codechallenge/a2048ather/Game.java
+++ app/src/main/java/com/ahmed/ather/codechallenge/a2048ather/Game.java
@@ -1,6 +1,8 @@
 package com.ahmed.ather.codechallenge.a2048ather;
 
 import java.util.HashMap;
+import java.util.Random;
+import static com.ahmed.ather.codechallenge.a2048ather.LogUtils.*;
 
 public class Game {
 
@@ -14,7 +16,7 @@ public class Game {
         return new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16" };
     }
 
-    void showBoard() {
+    public void showBoard() {
         String[] arr = getBoardKeySet();
 
         for (int i = 1; i <= arr.length; i++) {
@@ -23,7 +25,38 @@ public class Game {
                 System.out.println("");
             }
         }
+    }
 
+    String[] generateIndex(boolean generateTwoNumber) {
+        Random random1 = new Random(), random2 = new Random();
+        int r1 = random1.nextInt(16) + 1, r2 = random2.nextInt(16) + 1;
+
+        // To make sure r1 & r2 should not be same
+        if (r1 == r2 && r2 != 16) {
+            r2++;
+        } else if (r1 == r2) {
+            r2--;
+        }
+
+        String arr[] = new String[2];
+        arr[0] = r1 + "";
+        if (generateTwoNumber)
+            arr[1] = r2 + "";
+
+        showLog(r1 + " " + r2);
+        return arr;
+    }
+
+    String[] reverse(String arr[]) {
+
+        String[] localArr = new String[arr.length];
+        int n = localArr.length;
+        for (int i = 0; i < localArr.length; i++) {
+            localArr[n-1] = arr[i];
+            n-=1;
+        }
+
+        return localArr;
     }
 
 }
