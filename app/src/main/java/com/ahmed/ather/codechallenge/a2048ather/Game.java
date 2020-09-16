f60cf5eb69793d5cc52cb0ed600728787a5c3cea
ahmedmeeran.thamby <ahmedmeeran.thamby@tessolve.com>
2020-09-17 02:15:21 +0530

    Created Game Class for Business Logics. Created LogUtils class for logging purpose.

--- app/src/main/java/com/ahmed/ather/codechallenge/a2048ather/Game.java
+++ app/src/main/java/com/ahmed/ather/codechallenge/a2048ather/Game.java
@@ -1,4 +1,29 @@
 package com.ahmed.ather.codechallenge.a2048ather;
 
+import java.util.HashMap;
+
 public class Game {
+
+    private HashMap<String, Integer> map;
+
+    public Game(){
+        map = new HashMap<>();
+    }
+
+    public String[] getBoardKeySet() {
+        return new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16" };
+    }
+
+    void showBoard() {
+        String[] arr = getBoardKeySet();
+
+        for (int i = 1; i <= arr.length; i++) {
+            System.out.print(map.get(i + "") + " ");
+            if (i == 4 || i == 8 || i == 12 || i == 16) {
+                System.out.println("");
+            }
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/ahmed/ather/codechallenge/a2048ather/LogUtils.java
@@ -0,0 +1,31 @@
+package com.ahmed.ather.codechallenge.a2048ather;
+
+import android.util.Log;
+
+public class LogUtils {
+
+    public static void showLog(String msg){
+        Log.d("LOG",msg);
+    }
+
+    public static void showError(String msg){
+        Log.e("LOG",msg);
+    }
+
+    public static void showInfo(String msg){
+        Log.i("LOG",msg);
+    }
+
+    public static void showLog(String tag,String msg){
+        Log.d("LOG",msg);
+    }
+
+    public static void showError(String tag,String msg){
+        Log.e("LOG",msg);
+    }
+
+    public static void showInfo(String tag,String msg){
+        Log.i("LOG",msg);
+    }
+
+}
