eb42963914e6bdf9a787ef41c940ac71e33c63a0
ahmedmeeran.thamby <ahmedmeeran.thamby@tessolve.com>
2020-09-18 09:01:33 +0530

    Handled Game Won and Game Over Scenario and it's functionalities.

--- app/src/main/java/com/ahmed/ather/codechallenge/a2048ather/activity/MainActivity.java
+++ app/src/main/java/com/ahmed/ather/codechallenge/a2048ather/activity/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -43,10 +44,13 @@ public class MainActivity extends BaseActivity {
     CardView gameCardView;
     @BindView(R.id.game_over_layout) ConstraintLayout gameOverLayout;
     @BindView(R.id.content_title) TextView contentTitle;
+    @BindView(R.id.reset_button) Button resetButton;
 
-    BoardHelper boardHelper;
+    private BoardHelper boardHelper;
     public int gameScore;
+    private final int scoreToWin = 2048;
     private HashMap<String, Integer> map;
+    private OnSwipeTouchListener swipeTouchListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,19 +63,7 @@ public class MainActivity extends BaseActivity {
     private void initialSetup() {
         ButterKnife.bind(this);
         map = new HashMap<>();
-        boardHelper = new BoardHelper(new BoardHelper.ScoreFeed() {
-            @Override
-            public void updateScore(int score) {
-                showInfo("Points from Feed: "+score);
-                gameScore = gameScore + score;
-                MainActivity.this.updateScore(gameScore);
-            }
-        });
-        boardHelper.populateBoardMap(map);
-        String[] mapKey = boardHelper.generateIndex(true);
-        map.put(mapKey[0],2);
-        map.put(mapKey[1],2);
-        setListenerToLayouts(new OnSwipeTouchListener(MainActivity.this){
+        swipeTouchListener = new OnSwipeTouchListener(MainActivity.this){
             @Override
             public void onSwipeBottom() {
                 boardHelper.moveBoardsVertically(map,false);
@@ -103,9 +95,38 @@ public class MainActivity extends BaseActivity {
                 checkPossibleMove();
                 showInfo("onSwipeTop");
             }
+        };
+        boardHelper = new BoardHelper(new BoardHelper.ScoreFeed() {
+            @Override
+            public void updateScore(int score) {
+                showInfo("Points from Feed: "+score);
+                gameScore = gameScore + score;
+                MainActivity.this.updateScore(gameScore);
+                if (score == scoreToWin)
+                    gameWon();
+            }
         });
+        boardHelper.populateBoardMap(map);
+        String[] mapKey = boardHelper.generateIndex(true);
+        map.put(mapKey[0],2);
+        map.put(mapKey[1],2);
+        setListenerToLayouts(swipeTouchListener);
         bestScore.setText(getScore(KEY,"0"));
         onGridChanged(boardHelper.getMapKeyList());
+    }
+
+    public void resetGame(){
+        boardHelper.populateBoardMap(map);
+        String[] mapKey = boardHelper.generateIndex(true);
+        map.put(mapKey[0],2);
+        map.put(mapKey[1],2);
+        onGridChanged(boardHelper.getMapKeyList());
+        gameOverLayout.setVisibility(View.GONE);
+        gameOverLayout.setBackgroundColor(getColor(R.color.game_over_background));
+        contentTitle.setText("Game Over!");
+        contentTitle.setTextColor(getColor(R.color.cardview_dark_background));
+        resetButton.setVisibility(View.GONE);
+        setListenerToLayouts(swipeTouchListener);
     }
 
     private void updateScore(int score) {
@@ -126,6 +147,15 @@ public class MainActivity extends BaseActivity {
         }else{
             showInfo("There is Possibility because noOfEmptyTile is "+noOfEmptyTiles);
         }
+    }
+
+    private void gameWon(){
+        gameOverLayout.setBackgroundColor(getColor(R.color.game_won_background));
+        contentTitle.setTextColor(getColor(R.color.cardview_light_background));
+        contentTitle.setText("You win!");
+        gameOverLayout.setVisibility(View.VISIBLE);
+        resetButton.setVisibility(View.VISIBLE);
+        setListenerToLayouts(null);
     }
 
     void setListenerToLayouts(View.OnTouchListener listener){
@@ -224,5 +254,9 @@ public class MainActivity extends BaseActivity {
                 textView.setTextSize(19f);
                 break;
         }
+    }
+
+    public void onReset(View view) {
+        resetGame();
     }
 }
